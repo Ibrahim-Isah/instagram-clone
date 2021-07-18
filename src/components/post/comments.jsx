9d5374584d75bcacd0d1d7988bfc6e37def59a90
Isah Ibrahim <isahibn08@gmail.com>
2021-07-18 22:25:34 +0100

    added some reasonable functinality

--- src/components/post/comments.jsx
+++ src/components/post/comments.jsx
@@ -2,12 +2,16 @@ import { useState } from "react"
 import { Link } from "react-router-dom"
 import { formatDistance } from 'date-fns'
 import PropTypes from 'prop-types'
+import AddComment from "./AddComment"
 
 const Comments = ({
     docId,
     comments: allComments, posted , commentInput
 }) => {
-    const [comments, setComments] = useState(allComments)
+    const [comments, setComments] = useState(allComments);
+
+
+
     return (
         <>
             <div className="p-4 pt-1 pb-4">
@@ -16,18 +20,24 @@ const Comments = ({
                         View all {comments.length} comments
                     </p>
                 )}
-                {comments.slice(0,3).map((item)=>{
+                {comments.slice(0,3).map((item) => (
                     <p key={`${item.comment}-${item.displayName}`} className="mb-1">
                         <Link to={`/p/${item.displayName}`}>
                             <span className="mr-1 font-bold">{item.displayName}</span>
                         </Link>
                         <span>{item.comment}</span>
                     </p>
-                })}
+                ))}
                 <p className="text-gray-base uppercase text-xs mt-2">
                     {formatDistance(posted, new Date())} ago
                 </p>
             </div>
+            <AddComment 
+                docId={docId}
+                comments={comments}
+                setComments={setComments}
+                commentInput={commentInput}
+            />
         </>
     )
 }
