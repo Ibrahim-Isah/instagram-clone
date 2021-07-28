079f78a0b88d5f4bab527dd9df0c1d1a509d739a
Isah Ibrahim <isahibn08@gmail.com>
2021-07-28 22:44:31 +0100

    added some changes to the side bar

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,9 +1,10 @@
-import useUser from "../../hooks/use-user"
 import User from './user'
 import Suggestions from './suggestions'
+import { useContext } from 'react'
+import LoggedInUserContext from '../../context/logged-in-user'
 
 const Sidebar = () => {
-    const {user: { docId , fullName, username, userId , following}} = useUser()
+    const {user: { docId = '' , fullName, username, userId , following} = {}} = useContext(LoggedInUserContext)
     return (
         <div className="">
           <User username={username} fullName={fullName} />
