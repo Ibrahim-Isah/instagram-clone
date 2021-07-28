143e6548c95b76d61edfbff196df1decb6c37746
Isah Ibrahim <isahibn08@gmail.com>
2021-07-28 12:28:51 +0100

    completed version of the instagram clone

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,12 +1,12 @@
 import { useEffect } from "react"
-
-
+import Header from '../components/Header'
 export default function NotFound(){
     useEffect(() => {
         document.title = 'Not Found! - Instagram';
     }, [])
     return(
         <div className="bg-gray-background">
+            <Header />
             <div className="mx-auto max-w-screen-lg">
                 <p className="text-center text-2xl">Not Found!</p>
             </div>
