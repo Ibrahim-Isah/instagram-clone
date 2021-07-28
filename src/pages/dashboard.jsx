2511b8e84fcd7a25d85211a94b490c2435d306db
Isah Ibrahim <isahibn08@gmail.com>
2021-07-28 22:11:00 +0100

    added some changes to the dashboard

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -14,13 +14,16 @@ const Dashboard = ({user: loggedInUser}) => {
         
     }, [])
     return (
-        <div className="bg-gray-background">
-            <Header />
-            <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
-                <Timeline />
-                <Sidebar />
+        <LoggedInUserContext.Provider value={{ user }}>
+            <div className="bg-gray-background">
+                <Header />
+                <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+                    <Timeline />
+                    <Sidebar />
+                </div>
             </div>
-        </div>
+        </LoggedInUserContext.Provider>
+        
     )
 }
 
